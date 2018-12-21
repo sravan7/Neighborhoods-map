7ca5447fd7715529c517839983904eff26168fb3
sravan7 <esd14i017@iiitdm.ac.in>
2018-12-21 23:58:59 +0530

    Updates

---
+++ precache-manifest.c941fbd36e935c8ba3d0d2cdaa524f38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e26eb36d3028478ca0e",
+    "url": "/Neighborhoods-map/static/js/runtime~main.0e26eb36.js"
+  },
+  {
+    "revision": "04753435a8d6583e02fa",
+    "url": "/Neighborhoods-map/static/js/main.04753435.chunk.js"
+  },
+  {
+    "revision": "77958300264c6869a197",
+    "url": "/Neighborhoods-map/static/js/1.77958300.chunk.js"
+  },
+  {
+    "revision": "04753435a8d6583e02fa",
+    "url": "/Neighborhoods-map/static/css/main.8580a22f.chunk.css"
+  },
+  {
+    "revision": "ab003daff25a58b366d1b5129a403406",
+    "url": "/Neighborhoods-map/index.html"
+  }
+];
